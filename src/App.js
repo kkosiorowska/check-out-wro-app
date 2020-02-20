894fc994b60cf73fb5e8a01b90abf024588d05ad
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-20 15:06:24 +0100

    Adding first components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Toolbar from './components/Navigation/Toolbar/Toolbar';
+import MainContent from './containers/MainContent/MainContent';
 
 function App() {
   return (
     <div className="App">
-
+        <Toolbar/>
+        <MainContent/>
     </div>
   );
 }
---
+++ src/containers/MainContent/MainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class MainContent extends Component {
+
+    render () {
+        return (
+            <div>
+                MainContent
+            </div>
+        );
+    }
+}
+
+export default MainContent;
