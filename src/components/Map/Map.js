ceaf0c06dc8ae5afb2decf2d93aff087c72824b7
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-22 14:40:45 +0100

    Creating Map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 
-import classes from "./Map.css";
+import classes from './Map.css';
 
-export class Map extends Component {
-    render () {
-        return (
-            <div className={classes.Map}>
-                Map
-            </div>
-        );
-    }
-}
+const map = (props) => {
+    return (
+        <div className={classes.Map}>
+            <LeafletMap
+                center={[51.11, 17.03]}
+                zoom={14}
+                maxZoom={20}
+                attributionControl={true}
+                zoomControl={true}
+                doubleClickZoom={true}
+                scrollWheelZoom={true}
+                dragging={true}
+                animate={true}
+                easeLinearity={0.35} >
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+            </LeafletMap>
+        </div>
+    );
+};
 
-export default Map;
+export default map;
