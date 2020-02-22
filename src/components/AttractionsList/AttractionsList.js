54a2544571718b01f0530494dd9da76167465395
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-22 15:20:34 +0100

    Prepering example data

--- src/components/AttractionsList/AttractionsList.js
+++ src/components/AttractionsList/AttractionsList.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 import classes from'./AttractionsList.css';
+import Attraction from './Attraction/Attraction';
 
 export class AttractionsList extends Component {
     render () {
+        let attractions = this.props.attractions.map(row => (
+            <Attraction
+                key={row.name}
+                type={row.type}
+                imagePath={row.imagePath}
+                name={row.name}
+                shortDescription={row.shortDescription}
+                address={row.address}
+            />
+        ));
         return (
             <div className={classes.AttractionsList}>
-                Attractions List
+                {attractions}
             </div>
         );
     }
--- src/containers/MainContent/MainContent.js
+++ src/containers/MainContent/MainContent.js
@@ -4,10 +4,37 @@ import Map from '../../components/Map/Map';
 import AttractionsList from '../../components/AttractionsList/AttractionsList';
 
 export class MainContent extends Component {
+    state = {
+        attractions: [
+            {
+                type: "Landscape",
+                imagePath: "https://2.bp.blogspot.com/-xogZXTP4G0E/XLNSvIh8xvI/AAAAAAACHAE/uDKmis_QM5E_zE9HYtI74Q7XV1kvhZubwCKgBGAs/s1600/IMG_20190302_145018-01.jpeg",
+                name: "Kolorowe podwórko",
+                shortDescription: "Lokalna kronika mieszkańców Nadodrza. Szare podwórka zastały zamienione na kolorowe murale.",
+                address: "Franklina Delano Roosevelta 14-16"
+            },
+            {
+                type: "Architecture",
+                imagePath: "https://www.park-m.pl/images/news/czerwiec2017/Wroc%C5%82aw_Dunikowskiego_1.jpg",
+                name: "Bulwar Xawerego Dunikowskiego",
+                shortDescription: "Odnowiony bulwar przy nabrzeżu Odry. Miejsce do spacerowania i odpoczywania z pięknym widokiem na Odrę.",
+                address: "bulwar Xawerego Dunikowskiego"
+            },
+            {
+                type: "Gastronomy",
+                imagePath: "https://lh4.googleusercontent.com/proxy/ICMftI9284ph2gWSWA8FM-Yy6YBqiBXFxWo-u78zVgermPjDXJT3Ysn6p8ZGllaZUEQ2xIYgiBABNJHchFOTlM8kcEjwtFdWMq4_tH9bXAQEz7ToYe3yLd4j0Vb3jJw",
+                name: "Szyciokawiarnia Nr 1 - Dresówka.pl",
+                shortDescription: "W kawiarnii możesz skorzystać z dostępnych na miejscu maszyn i wykrojów by uszyć swoje wymarzone kreacje pod okiem pomocnej załogi.",
+                address: "Wyszyńskiego 39"
+            }
+        ]
+    };
+
     render () {
         return (
             <div>
-                <AttractionsList />
+                <AttractionsList
+                    attractions={this.state.attractions}/>
                 <Map/>
             </div>
         );
