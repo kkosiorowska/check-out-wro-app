1cd6e3fd9f9da8d9288a36a78955de7b153428fc
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-02-27 13:05:23 +0100

    Updating AttractionDetails component

--- src/components/Attractions/AttractionDetails/AttractionDetails.js
+++ src/components/Attractions/AttractionDetails/AttractionDetails.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './AttractionDetails.css';
+import Button from '../../UI/Button/Button';
+import leftArrow from "../../../assets/images/left-arrow .png";
 
-export class AttractionDetails extends Component {
-    render () {
+const attractionDetails = (props) => {
+    const attraction = props.attractions.find(({ id }) => id === props.match.params.attractionId);
         return (
             <div className={classes.AttractionDetails}>
-                Attraction Details
+                <div className={classes.ImageContainer}>
+                    <Button
+                        clicked={props.clicked}
+                        btnType="Back">
+                        <img src={leftArrow} alt="LeftArrow"/>
+                    </Button>
+                    <img src={attraction.imagePath} alt="imageAttraction"/>
+                </div>
+                <div className={classes.DetailsContainer}>
+                    <p><strong>Name:</strong> {attraction.name} </p>
+                    <hr/>
+                    <p><strong>Category:</strong> {attraction.type} </p>
+                    <p><strong>District:</strong> {attraction.district} </p>
+                    <p><strong>Address:</strong> {attraction.address} </p>
+                    <hr/>
+                    <p><strong>Description:</strong> {attraction.description} </p>
+                </div>
             </div>
         );
-    }
-}
+};
 
-export default AttractionDetails;
+export default attractionDetails;
